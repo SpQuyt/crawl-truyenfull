791d0f0c30685f207a691d73175401f2fe2a8729
SpQuyt <spquyt@gmail.com>
2019-05-31 20:27:57 +0700

    getStoryInfo on browser worked well; but fail while calling from ReactJS

--- helpers/Crawler/TruyenYY.js
+++ helpers/Crawler/TruyenYY.js
@@ -14,23 +14,36 @@ const sleep = (ms) => {
 }
 
 class TruyenYY {
-
   static async crawl1Page(title, index) {
     try {
       const res = await https.get(`${truyenYYURL}truyen/${title}/chuong-${index}.html`);
       const ele = parser.parseFromString(res.text, 'text/html');
       const dom = new JSDOM(ele.rawHTML);
 
-      console.log (dom.window.document.getElementsByClassName('chap-content serif-font')[0].innerHTML.replace('\n', ' ')); 
-      
+      const header = dom.window.document.getElementsByClassName('chap-title sans-serif-font')[0].innerHTML
+        .replace(/<span>/g, '')
+        .replace(/<\/span>/g, ' - ');
+      const body = Array.from(dom.window.document
+        .getElementsByClassName('chap-content serif-font')[0]
+        .getElementsByTagName('p'));
+
+      body.map((paragraph, index, body) => {
+        body[index] = body[index].innerHTML.replace(/\n/g, ' ');
+      })
+
+      console.log(header);
+
+      return {
+        header: header,
+        body: body,
+      }
+
     } catch (err) {
       if (err.status == 503) {
         console.log('Too many requests!');
-      }
-      else if (err.status == 404) {
+      } else if (err.status == 404) {
         console.log(`Story ${title} not found!`);
-      }
-      else {
+      } else {
         console.log(err);
       }
     }
--- server.js
+++ server.js
@@ -6,14 +6,15 @@ import TruyenYY from './helpers/Crawler/TruyenYY';
 import MyRegEx from './helpers/MyRegEx';
 
 const app = express();
-const port = 3000;
+const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 
-app.get('/download', (req, res) => {
-  const file = `${__dirname}/downloadable/tien-nghich/truyenfull/tien-nghich.docx`;
-  res.download(file);
+app.get(`/GetStoryInfo`, async (req, res) => {
+  console.log("U GUD")
+  let result =  await Truyenfull.crawl1Chapter(req.query.category, 1019);
+  res.send(result);
 })
 
 // console.log(Truyenfull.crawlAllChapters('tien-nghich', 1017, 1019));
@@ -26,10 +27,10 @@ app.get('/download', (req, res) => {
 // Truyenfull.writeDoc('linh-vu-thien-ha', 1017, 1019);
 // myregex.convertUTF8('Dị Nhân Tu Chân Đa Thế Giới');
 // TruyenCV.crawl1Page('tien-nghich', 1000);
-TruyenYY.crawl1Page('tien-nghich', 1020);
+// TruyenYY.crawl1Page('tien-nghich', 1020);
 
 app.listen(port, (err) => {
-  if (err) throw err;
+  if (err) console.log(err);
   console.log(`Server started on port ${port}`);
 })
 
